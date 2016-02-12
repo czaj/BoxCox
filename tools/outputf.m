c03e73ded19ff1bed90fe56bf7c77731040d2e79
czaj <miq@wne.uw.edu.pl>
2016-02-12 08:12:56 +0100

    Output function change

--- tools/outputf.m
+++ tools/outputf.m
@@ -1,31 +1,28 @@
 function stop = outputf(x,optimvalues,state)
-global B_backup
-% global tolB
-persistent LL_backup
+global B_backup 
+persistent LL_backup IterTime
 
 stop = false;
-% tolB = 1.e-6;
 if isequal(state,'init')
     disp('')
-	fprintf('%6s %6s %8s %16s %17s %18s %17s\n','Iter.','Eval.','dB','Step','f(x)','df(x)','Opt. Cond.');
-end
-    if isequal(state,'iter')
-        if optimvalues.iteration == 0
-            if isempty(optimvalues.stepsize)
-                optimvalues.stepsize = 0;
-            end
-            fprintf('%4d %6d %15.10f %15.10f %19.10f %15.10f %15.10f\n',optimvalues.iteration,optimvalues.funccount,0,optimvalues.stepsize,optimvalues.fval,0,optimvalues.firstorderopt);
-            B_backup = x;
-            LL_backup = optimvalues.fval;
-        else
-            dB = max(abs(x - B_backup));
-            fprintf('%4d %6d %15.10f %15.10f %19.10f %15.10f %15.10f\n',optimvalues.iteration,optimvalues.funccount,dB,optimvalues.stepsize,optimvalues.fval,LL_backup - optimvalues.fval,optimvalues.firstorderopt);
-            B_backup = x;
-            LL_backup = optimvalues.fval;
-%             if dB < tolB 
-%                 stop = true;
-%                 disp(['Exiting optimalization due to change in parameters is lower than selected tolerance (',num2str(tolB),')'])
-%             end
+	fprintf('%6s %6s %8s %16s %17s %18s %17s %12s \n','Iter.','Eval.','dB','Step','f(x)','df(x)','Opt. Cond.','Iter. time');
+elseif isequal(state,'iter')
+    IterTocNote = toc(IterTime);
+    if optimvalues.iteration == 0
+        if isempty(optimvalues.stepsize)
+            optimvalues.stepsize = 0;
         end
+        fprintf('%4d %6d %15.10f %15.10f %19.10f %15.10f %15.10f %9.4f\n',optimvalues.iteration,optimvalues.funccount,0,optimvalues.stepsize,optimvalues.fval,0,optimvalues.firstorderopt,IterTocNote);
+        B_backup = x;
+        LL_backup = optimvalues.fval;
+    else
+        dB = max(abs(x - B_backup));
+        fprintf('%4d %6d %15.10f %15.10f %19.10f %15.10f %15.10f %9.4f\n',optimvalues.iteration,optimvalues.funccount,dB,optimvalues.stepsize,optimvalues.fval,LL_backup - optimvalues.fval,optimvalues.firstorderopt,IterTocNote);
+        B_backup = x;
+        LL_backup = optimvalues.fval;
     end
 end
+
+IterTime = tic;
+
+
